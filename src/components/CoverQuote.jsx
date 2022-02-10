6a04ff629953a2507f2934b33f3668c9d27ce9de
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-10 22:08:46 +0100

    update cover quote

--- src/components/CoverQuote.jsx
+++ src/components/CoverQuote.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CoverQuote = () => {
 	return (
-		<blockquote className="cover-quote w-400px white pd-l-40 pd-r-40 p-n n-s">
+		<blockquote className="cover-quote w-400px white pd-l-40 pd-r-40 p-n n-s pd-b-20 pd-t-20 fs-120">
 			The wind rises, we must try to live.
 			<footer>
 				<cite>Paul Valéry</cite>
