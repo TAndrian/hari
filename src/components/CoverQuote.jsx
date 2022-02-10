cda6e87618d9889a6c19fa18faa43aac7d024261
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-10 16:48:53 +0100

    add bottom cover quote

---
+++ src/components/CoverQuote.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CoverQuote = () => {
+	return (
+		<blockquote className="cover-quote w-400px white pd-l-40 pd-r-40 p-n n-s">
+			The wind rises, we must try to live.
+			<footer>
+				<cite>Paul Valéry</cite>
+			</footer>
+		</blockquote>
+	);
+};
+
+export default CoverQuote;
