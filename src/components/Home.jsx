d75ab8d9286bfda7c366e2fa2c59e8868a894f66
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-11 17:36:39 +0100

    add section component layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Navbar from "./Navbar";
 import Page from "./Page";
 import Cover from "./Cover";
+import Section from "./Section";
 
 /*
 	Home page
@@ -12,7 +13,15 @@ const Home = () => (
 		<Navbar />
 		<Page>
 			<Cover />
-			<h1>Hello from portfolio v3 !</h1>
+			<Section
+				id="journey"
+				title="A passionnate developer"
+				subtitle="Let me tell a story"
+			>
+				<Fragment>
+					I have some experience in the software engineering. Here let me tell you a tale, a of a passionate developer in the world of programmation. Everything I have done since then is to learn again and again, trying to build apps successfully by myself though tutorials. Yeah I&apos;m self taught, I didn&apos;t had the opportunity to finish my studies but I swear I&apos;m more than skilled to do the job ! Nowadays, a bunch of new technologies are released every year and programming languages evolve rapidly with new features. So, learning and mastering quickly new frameworks or libraries is a key for a successfull developer. These tools are meant to ease the development process, to build better apps with less code and more structurated patterns.
+				</Fragment>
+			</Section>
 		</Page>
 	</>
 );
