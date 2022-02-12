02495fe1e29163c150307283cbd2b6554f6cb8d1
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-12 14:13:54 +0100

    add meet section

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import GetInTouchTitle from "./GetInTouchTitle";
+import MovingClouds from "./MovingClouds";
 
 const Games = () => {
 	return (
 		<div className="games f-c-st-st n-s">
-			<GetInTouchTitle title="Other projects"/>
+			<GetInTouchTitle title="Let's take a coffe" icon={ ["fas", "coffee"] }/>
 			<div
-				className="display b-b br-5 o-h mg-t-20 mg-l-20 f-c-ce-ce"
+				className="games__box display b-b br-5 o-h mg-t-20 mg-l-20 f-c-ce-ce"
 				style={{
 					background: "url(/assets/games.jpg) center center no-repeat",
 					backgroundSize: "cover"
 				}}
 			>
-				<p className="white mg-b-20 n-s p-n">I&apos;m also a game developer !</p>
-				<a className="checkout theme br-5 pd-10 pd-l-20 pd-r-20 tr-200 p">
-					Check out
-				</a>
+				<MovingClouds />
+				<div className="games__content f-c-ce-ce w-100 h-100">
+					<p className="text white mg-b-20 n-s p-n pd-10 pd-t-5 pd-b-5 br-5">
+						Interested ? Let&apos;s meet or video call !<br />
+						Check my resume bellow
+					</p>
+					<a
+						className="checkout theme br-5 pd-10 pd-l-20 pd-r-20 tr-200 p"
+						href="/Resume Harijaona Rajaonson.pdf"
+						download="Resume Harijaona Rajaonson"
+					>
+						<Icon icon={ ["fas", "download"] }/> Download resume
+					</a>
+				</div>
 			</div>
 		</div>
 	);
