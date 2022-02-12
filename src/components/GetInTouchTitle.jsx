103ba9abfd00ce304654444708f44749a356d07d
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-12 14:13:26 +0100

    get in touch title now accepts icons

--- src/components/GetInTouchTitle.jsx
+++ src/components/GetInTouchTitle.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 
-const GetInTouchTitle = ({ title }) => (
+const GetInTouchTitle = ({ title, icon }) => (
 	<div className="get-in-touch-title n-s">
-		<h2 className="theme pd-20">{ title || "Title" }</h2>
+		<h2 className="theme pd-20">
+			{icon && <Icon icon={ icon } />} { title || "Title" }
+		</h2>
 		<div className="separator mg-l-20"></div>
 	</div>
 );
