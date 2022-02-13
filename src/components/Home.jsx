366e92d05e83d794cc5aa2a3caed2f4d7264a73f
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-13 14:42:22 +0100

    minor edits

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import Journey from "./Journey";
 import Copyright from "./Copyright";
 import GetInTouch from "./GetInTouch";
 import ProjectCard from "./ProjectCard";
+import ProjectFilter from "./ProjectFilter";
 
 /*
 	Home page
@@ -36,6 +37,7 @@ const Home = () => (
 				As a developer, I already achieved many personal and professional projects to make use of my skills. These skills helped me a lot to realize such refined applications. Some of them could appear simple, but the logic behind the stack it uses is the major part of it is hidden from the public eye. If you want to know more about, I invite you to take a look at my Github repositories.
 			</Fragment>
 		</Section>
+		<ProjectFilter />
 		<div className="container">
 			<ProjectCard />
 		</div>
