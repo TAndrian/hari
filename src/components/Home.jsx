d7ffe17050f89c5aad333ce401d825bcc9af1d10
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-13 02:04:46 +0100

    update titles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import Section from "./Section";
 import Journey from "./Journey";
 import Copyright from "./Copyright";
 import GetInTouch from "./GetInTouch";
+import ProjectCard from "./ProjectCard";
 
 /*
 	Home page
@@ -26,7 +27,19 @@ const Home = () => (
 		<div className="container">
 			<Journey />
 		</div>
-		<GetInTouch />
+		<Section
+			id="projects"
+			title="Creativity to another level"
+			subtitle="My awesome projects"
+		>
+			<Fragment>
+				As a developer, I already achieved many personal and professional projects to make use of my skills. These skills helped me a lot to realize such refined applications. Some of them could appear simple, but the logic behind the stack it uses is the major part of it is hidden from the public eye. If you want to know more about, I invite you to take a look at my Github repositories.
+			</Fragment>
+		</Section>
+		<div className="container">
+			<ProjectCard />
+		</div>
+		{/*<GetInTouch />*/}
 		<Copyright />
 	</Fragment>
 );
