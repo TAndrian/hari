ba0c3a288321c09c4bec94f02f46f1e1022a9155
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-13 02:04:14 +0100

    update quotes

--- src/components/CoverQuote.jsx
+++ src/components/CoverQuote.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const CoverQuote = () => {
 	return (
 		<blockquote className="cover-quote w-380px white pd-l-40 pd-r-40 p-n n-s pd-b-20 pd-t-20 fs-120">
-			The wind rises, we must try to live.
+			Imagination is more important than intelligence.
 			<footer>
-				<cite>Paul Valéry</cite>
+				<cite>Albert Einstein</cite>
 			</footer>
 		</blockquote>
 	);
