dbce4508f8f6a85ac8390f8c7ccf8b2f6af5612b
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-14 11:21:44 +0100

    add pricing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,10 @@ const title = "Harijaona";
 const splittedTittle = title.split("");
 
 const linkList = [
-	{ icon: ["fas", "user"], content: "Me", anchor: "#0", title: "Who am I ?" },
 	{ icon: ["fas", "pencil-alt"], content: "My journey", anchor: "#0", title: "Read my story." },
 	{ icon: ["fas", "wrench"], content: "Skillset", anchor: "#0", title: "How skilled I am ?" },
 	{ icon: ["fas", "laptop"], content: "Projects", anchor: "#0", title: "List of my projects." },
+	{ icon: ["fas", "id-card"], content: "Pricing", anchor: "#0", title: "View my prices" },
 	{ icon: ["fas", "envelope"], content: "Contacts", anchor: "#0", title: "How to reach me." }
 ];
 
